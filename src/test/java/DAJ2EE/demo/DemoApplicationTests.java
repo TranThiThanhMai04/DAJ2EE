ebf3d34e6b6c8545f238a94a6852b3b64849b473
Thanh Mai <mait6464@gmail.com>
2026-03-18 20:51:57 +0700

    Initial commit

---
+++ src/test/java/DAJ2EE/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package DAJ2EE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
